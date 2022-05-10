c367293aeb1cc7840caefc5492f02b99856eec76
amit kumar <71318008+amitkumar70512@users.noreply.github.com>
2022-05-10 14:13:10 +0530

    Update ID3.py

--- Lab3/ID3.py
+++ Lab3/ID3.py
@@ -1,108 +1,91 @@
 
-# Importing the required packages
-import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+import math
+import numpy as np
 
-# Function importing Dataset
-def importdata():
-	balance_data = pd.read_csv(
-'https://archive.ics.uci.edu/ml/machine-learning-'+
-'databases/balance-scale/balance-scale.data',
-	sep= ',', header = None)
-	
-	# Printing the dataswet shape
-	print ("Dataset Length: ", len(balance_data))
-	print ("Dataset Shape: ", balance_data.shape)
-	
-	# Printing the dataset obseravtions
-	print ("Dataset: ",balance_data.head())
-	return balance_data
+data = pd.read_csv("3-dataset.csv")
+features = [feat for feat in data]
+features.remove("answer")
 
-# Function to split the dataset
-def splitdataset(balance_data):
+class Node:
+    def __init__(self):
+        self.children = []
+        self.value = ""
+        self.isLeaf = False
+        self.pred = ""
 
-	# Separating the target variable
-	X = balance_data.values[:, 1:5]
-	Y = balance_data.values[:, 0]
+def entropy(examples):
+    pos = 0.0
+    neg = 0.0
+    for _, row in examples.iterrows():
+        if row["answer"] == "yes":
+            pos += 1
+        else:
+            neg += 1
+    if pos == 0.0 or neg == 0.0:
+        return 0.0
+    else:
+        p = pos / (pos + neg)
+        n = neg / (pos + neg)
+        return -(p * math.log(p, 2) + n * math.log(n, 2))
 
-	# Splitting the dataset into train and test
-	X_train, X_test, y_train, y_test = train_test_split(
-	X, Y, test_size = 0.3, random_state = 100)
-	
-	return X, Y, X_train, X_test, y_train, y_test
-	
-# Function to perform training with giniIndex.
-def train_using_gini(X_train, X_test, y_train):
+def info_gain(examples, attr):
+    uniq = np.unique(examples[attr])
+    #print ("\n",uniq)
+    gain = entropy(examples)
+    #print ("\n",gain)
+    for u in uniq:
+        subdata = examples[examples[attr] == u]
+        #print ("\n",subdata)
+        sub_e = entropy(subdata)
+        gain -= (float(len(subdata)) / float(len(examples))) * sub_e
+        #print ("\n",gain)
+    return gain
 
-	# Creating the classifier object
-	clf_gini = DecisionTreeClassifier(criterion = "gini",
-			random_state = 100,max_depth=3, min_samples_leaf=5)
+def ID3(examples, attrs):
+    root = Node()
 
-	# Performing training
-	clf_gini.fit(X_train, y_train)
-	return clf_gini
-	
-# Function to perform training with entropy.
-def tarin_using_entropy(X_train, X_test, y_train):
+    max_gain = 0
+    max_feat = ""
+    for feature in attrs:
+        #print ("\n",examples)
+        gain = info_gain(examples, feature)
+        if gain > max_gain:
+            max_gain = gain
+            max_feat = feature
+    root.value = max_feat
+    #print ("\nMax feature attr",max_feat)
+    uniq = np.unique(examples[max_feat])
+    #print ("\n",uniq)
+    for u in uniq:
+        #print ("\n",u)
+        subdata = examples[examples[max_feat] == u]
+        #print ("\n",subdata)
+        if entropy(subdata) == 0.0:
+            newNode = Node()
+            newNode.isLeaf = True
+            newNode.value = u
+            newNode.pred = np.unique(subdata["answer"])
+            root.children.append(newNode)
+        else:
+            dummyNode = Node()
+            dummyNode.value = u
+            new_attrs = attrs.copy()
+            new_attrs.remove(max_feat)
+            child = ID3(subdata, new_attrs)
+            dummyNode.children.append(child)
+            root.children.append(dummyNode)
+    return root
 
-	# Decision tree with entropy
-	clf_entropy = DecisionTreeClassifier(
-			criterion = "entropy", random_state = 100,
-			max_depth = 3, min_samples_leaf = 5)
+def printTree(root: Node, depth=0):
+    for i in range(depth):
+        print("\t", end="")
+    print(root.value, end="")
+    if root.isLeaf:
+        print(" -> ", root.pred)
+    print()
+    for child in root.children:
+        printTree(child, depth + 1)
 
-	# Performing training
-	clf_entropy.fit(X_train, y_train)
-	return clf_entropy
-
-
-# Function to make predictions
-def prediction(X_test, clf_object):
-
-	# Predicton on test with giniIndex
-	y_pred = clf_object.predict(X_test)
-	print("Predicted values:")
-	print(y_pred)
-	return y_pred
-	
-# Function to calculate accuracy
-def cal_accuracy(y_test, y_pred):
-	
-	print("Confusion Matrix: ",
-		confusion_matrix(y_test, y_pred))
-	
-	print ("Accuracy : ",
-	accuracy_score(y_test,y_pred)*100)
-	
-	print("Report : ",
-	classification_report(y_test, y_pred))
-
-# Driver code
-def main():
-	
-	# Building Phase
-	data = importdata()
-	X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
-	clf_gini = train_using_gini(X_train, X_test, y_train)
-	clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
-	
-	# Operational Phase
-	print("Results Using Gini Index:")
-	
-	# Prediction using gini
-	y_pred_gini = prediction(X_test, clf_gini)
-	cal_accuracy(y_test, y_pred_gini)
-	
-	print("Results Using Entropy:")
-	# Prediction using entropy
-	y_pred_entropy = prediction(X_test, clf_entropy)
-	cal_accuracy(y_test, y_pred_entropy)
-	
-	
-# Calling main function
-if __name__=="__main__":
-	main()
+root = ID3(data, features)
+printTree(root)
